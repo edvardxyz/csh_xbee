45e451e75eb7726e08043cf2aa01b770d1df1fe7
edvard <edvard@space-inventor.com>
2024-12-03 13:39:26 +0100

    xbee rx updated

--- src/xbee.c
+++ src/xbee.c
@@ -19,21 +19,28 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include "csp/autoconfig.h"
+
+typedef enum {
+	XBEE_MODE_NOT_STARTED,
+	XBEE_MODE_LENGTH_MSB,
+	XBEE_MODE_LENGTH_LSB,
+	XBEE_MODE_SKIP_FRAMING,
+	XBEE_MODE_RECEIVING,
+} xbee_receive_state_t;
 
 void send_16bit_frame(uint16_t dest_address, void * driver_data, const uint8_t * payload, uint16_t len);
 
 typedef struct {
-	int rx_linbuf_ctr;
-	char rx_linbuf[400];
-	csp_iface_t * iface;
-} xbee_ud_t;
-
-typedef struct {
 	char name[CSP_IFLIST_NAME_MAX + 1];
 	csp_iface_t iface;
-	xbee_ud_t ifdata;
-	pthread_t rx_thread;
 	int fd;
+	csp_packet_t * rx_packet;
+	xbee_receive_state_t xbee_mode;
+	uint16_t data_index;
+	uint16_t length;
+	uint8_t checksum;
+	bool escape_next;
 } xbee_ctx_t;
 
 void libinfo(void) {
@@ -108,135 +115,106 @@ int xbee_driver_tx(void * driver_data, uint32_t id, const uint8_t * data, uint8_
 	return CSP_ERR_NONE;
 }
 
-typedef enum {
-	XBEE_MODE_NOT_STARTED,
-	XBEE_MODE_LENGTH_MSB,
-	XBEE_MODE_LENGTH_LSB,
-	XBEE_MODE_RECEIVING,
-} xbee_receive_state_t;
-
 void xbee_driver_rx(void * user_data, uint8_t * data, size_t data_size, void * pxTaskWoken) {
-	// printf("inside isr\n");
-	static xbee_receive_state_t xbee_mode = XBEE_MODE_NOT_STARTED;
-	static uint8_t frame_data[256];
-	static uint16_t data_index = 0;
-	static uint16_t length = 0;
-	static uint8_t checksum = 0;
-	static bool escape_next = false;
+	xbee_ctx_t * ctx = (xbee_ctx_t *)user_data;
 
 	for (uint16_t i = 0; i < data_size; i++) {
 		printf("%02X ", data[i]);
 	}
 	printf("\n");
-//		7E 00 0A 81 00 02 14 00 68 65 6C 6C 6F 54
 
 	for (int i = 0; i < data_size; i++) {
 		if (data[i] == 0x7D) {
-			escape_next = true;  // Next byte is escaped
-			return;              // Wait for next byte
+			ctx->escape_next = true;  // Next byte is escaped
+			return;                   // Wait for next byte
 		}
 
-		if (escape_next) {
+		if (ctx->escape_next) {
 			data[i] ^= 0x20;  // Unescape the byte
-			escape_next = false;
+			ctx->escape_next = false;
 		}
 
-		switch (xbee_mode) {
+		switch (ctx->xbee_mode) {
 			case XBEE_MODE_NOT_STARTED:
 				if (data[i] == 0x7E) {
 					// Start delimiter detected
-					xbee_mode = XBEE_MODE_LENGTH_MSB;
-					length = 0;
-					data_index = 0;
-					checksum = 0;
+					csp_id_setup_rx(ctx->rx_packet);
+					ctx->xbee_mode = XBEE_MODE_LENGTH_MSB;
+					ctx->length = 0;
+					ctx->data_index = 0;
+					ctx->checksum = 0;
 				}
 				break;
 
 			case XBEE_MODE_LENGTH_MSB:
-				length = data[i] << 8;  // Store MSB of length
-				xbee_mode = XBEE_MODE_LENGTH_LSB;
+				ctx->length = data[i] << 8;  // Store MSB of length
+				ctx->xbee_mode = XBEE_MODE_LENGTH_LSB;
 				break;
 
 			case XBEE_MODE_LENGTH_LSB:
-				length |= data[i];  // Store LSB of length
-				if (length > sizeof(frame_data)) {
-					// Length too big, discard frame
-					xbee_mode = XBEE_MODE_NOT_STARTED;
+				ctx->length |= data[i];  // Store LSB of length
+				if (ctx->length > CSP_BUFFER_SIZE) {
+					ctx->iface.rx_error++;
+					ctx->iface.drop++;
+					ctx->xbee_mode = XBEE_MODE_NOT_STARTED;
 				} else {
-					xbee_mode = XBEE_MODE_RECEIVING;
+					ctx->xbee_mode = XBEE_MODE_SKIP_FRAMING;
+				}
+				break;
+			case XBEE_MODE_SKIP_FRAMING:
+				ctx->checksum += data[i];
+				ctx->data_index++;
+
+				if (ctx->data_index >= 4) {
+					ctx->xbee_mode = XBEE_MODE_RECEIVING;
 				}
 				break;
 
 			case XBEE_MODE_RECEIVING:
-
-//		7E 00 0A 81 00 02 14 00 68 65 6C 6C 6F 54
-				if (data_index < length) {
-					// Accumulate checksum and store data
-					checksum += data[i];
-					frame_data[data_index++] = data[i];
+				//		7E 00 0A 81 00 02 14 00 68 65 6C 6C 6F 54
+				if (ctx->data_index < ctx->length) {
+					ctx->checksum += data[i];
+					ctx->rx_packet->frame_begin[ctx->rx_packet->frame_length++] = data[i];  // xd
 				} else {
 					// Last byte is the checksum
-					uint8_t calculated_checksum = 0xFF - checksum;
+					uint8_t calculated_checksum = 0xFF - ctx->checksum;
 					if (calculated_checksum == data[i]) {
-						// Checksum valid, process the frame
-						// process_xbee_frame(frame_data, length);
-						// memcpy(frame_buf_in, frame_data, length);
-						// frame_in_len = length;
-						//
-						// Check frame type
-						if (frame_data[0] == 0x81) {                                         // Receive Packet Frame Type
-							uint16_t source_address = (frame_data[1] << 8) | frame_data[2];  // 16-bit source address
-							uint8_t rssi = frame_data[3];                                    // RSSI
-							uint8_t options = frame_data[4];                                 // Options
-							const uint8_t * rf_data = &frame_data[5];                        // Start of RF Data
 
-							uint16_t rf_data_length = length - 5;  // Exclude source, RSSI, and options fields
+						uint16_t frame_len = ctx->rx_packet->frame_length;
+						ctx->iface.rxbytes += frame_len;
+						ctx->iface.rx++;
 
-							printf("Source Address: 0x%04X\n", source_address);
-							printf("RSSI: -%d dBm\n", rssi);
-							printf("Options: 0x%02X\n", options);
-							printf("RF Data (HEX): ");
-							for (uint16_t i = 0; i < rf_data_length; i++) {
-								printf("%02X ", rf_data[i]);
-							}
-							printf("\n");
-
-							// If RF data contains a frame, process it further
-							if (rf_data[0] == 0x7E) {  // Start delimiter of an embedded frame
-								printf("Embedded Frame Detected!\n");
-								// Parse embedded frame as needed
-							}
+						printf("RF Data (HEX): ");
+						for (uint16_t i = 0; i < frame_len; i++) {
+							printf("%02X ", ctx->rx_packet->frame_begin[i]);
 						}
-						/*
-						if (csp_id_strip(ifdata->rx_packet) < 0) {
-							iface->frame++;
-							ifdata->rx_mode = KISS_MODE_NOT_STARTED;
+						printf("\n");
+
+						if (csp_id_strip(ctx->rx_packet) < 0) {
+							ctx->iface.frame++;
+							ctx->xbee_mode = XBEE_MODE_NOT_STARTED;
 							break;
 						}
-						// Send back into CSP, notice calling from task so last argument must be NULL!
-						csp_qfifo_write(ifdata->rx_packet, iface, pxTaskWoken);
-						*/
 
-						// for (uint16_t i = 0; i < length; i++) {
-						//  printf("%02X ", frame_data[i]);
-						//}
-						// printf("\n\n");
+						csp_qfifo_write(ctx->rx_packet, &ctx->iface, pxTaskWoken);
 					} else {
-						printf("invalid calc %u != got %u\n ",calculated_checksum, data[i]);
+						ctx->iface.rx_error++;
+						ctx->iface.drop++;
+						printf("invalid calc %u != got %u\n ", calculated_checksum, data[i]);
 						// Checksum invalid, handle error
 					}
 					// Reset state for next frame
-					xbee_mode = XBEE_MODE_NOT_STARTED;
+					ctx->xbee_mode = XBEE_MODE_NOT_STARTED;
 				}
 				break;
 
 			default:
-				xbee_mode = XBEE_MODE_NOT_STARTED;
+				ctx->iface.rx_error++;
+				ctx->iface.drop++;
+				ctx->xbee_mode = XBEE_MODE_NOT_STARTED;
 				break;
 		}
 	}
-	// printf("got %02X, mode = %d\n", data, xbee_mode);
-	//  Handle escape character
 }
 
 static int csp_ifadd_xbee_cmd(struct slash * slash) {
@@ -286,25 +264,18 @@ static int csp_ifadd_xbee_cmd(struct slash * slash) {
 		.stopbits = 1,
 		.paritysetting = 0};
 
-	xbee_ud_t * usart_ud = calloc(1, sizeof(*usart_ud));
-
 	strncpy(ctx->name, name, sizeof(ctx->name) - 1);
 	ctx->iface.name = ctx->name;
 	ctx->iface.addr = addr;
 	ctx->iface.netmask = mask;
 	ctx->iface.is_default = dfl;
-	ctx->iface.interface_data = &ctx->ifdata;
 	ctx->iface.driver_data = ctx;
 	ctx->iface.nexthop = csp_xbee_tx;
-	usart_ud->iface = &ctx->iface;
 
-	csp_usart_fd_t fd;
-
-	if (csp_usart_open(&conf, xbee_driver_rx, usart_ud, &fd) < 0) {
+	if (csp_usart_open(&conf, xbee_driver_rx, ctx, &ctx->fd) < 0) {
 		printf("csp_usart_open() failed\n");
 		return SLASH_SUCCESS;
 	}
-	ctx->fd = fd;
 	csp_iflist_add(&ctx->iface);
 
 	return SLASH_SUCCESS;
